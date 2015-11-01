ad354fc22ca6bdf2c855d62d098a434675c10f22
RJDH <rickjdhahn@gmail.com>
2015-10-31 20:26:22 -0400

    TestSecond

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="theme.css">
+<title>Second Page Title</title>
+</head>
+<body>
+
+<h1>This is a Second Heading</h1>
+<p>This is a second paragraph.</p>
+
+</body>
+</html> 
